d8f6cc27f35cf35e7a31a5d9085b2977d982ea51
gammpei <gammpei@gmail.com>
2018-02-18 21:54:51 -0500

    Add GPL-3.0-or-later license.

--- cpuOperations.go
+++ cpuOperations.go
@@ -1,3 +1,23 @@
+/*
+ * gammaboy is a Game Boy emulator.
+ * Copyright (C) 2018  gammpei
+ *
+ * This file is part of gammaboy.
+ *
+ * gammaboy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gammaboy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gammaboy.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package main
 
 // --------------------------
